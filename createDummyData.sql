7a0b82be1b3252d91b5dca9fb1f14323889221f5
ross <ross@cent285sp11.(none)>
2011-03-10 20:47:18 -1000

    fixed users and dummydata

--- createDummyData.sql
+++ createDummyData.sql
@@ -1,9 +1,23 @@
 -- createDummyData.sql a script to load some testing data
 -- make sure addInsertFuncs.sql is run before this
 
--- add a user
---insert into suggestions_view (title, body)
---values ('help','i am lost');
+insert into sugg_user_view (userid, body)
+values ('1','i am ross');
 
 insert into sugg_user_view (userid, body)
-values ('1','i am lost');
+values ('2','The West Division needs more applesauce');
+
+insert into sugg_user_view (userid, body)
+values ('3','The East Chair needs more salary');
+
+insert into sugg_user_view (userid, body)
+values ('4','Department One lacks chairs');
+
+insert into sugg_user_view (userid, body)
+values ('5','Department Two is quite sunny');
+
+insert into sugg_user_view (userid, body)
+values ('6','A Suggestion');
+
+insert into sugg_user_view (userid, body)
+values ('7','Yatta!');
